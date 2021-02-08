2282d5e5ccc50d144a22f7f49d5f45991fcd0d36
Levan96 <levan.mebonia.it@gmail.com>
2021-02-08 01:44:45 +0100

    p2p video

---
+++ public/script.js
@@ -0,0 +1,199 @@
+window.onload = () =>
+{
+  let constraints = {video: true, audio: true};
+  let peerID = "";
+  let roomLog = {};
+  // -------------------- home page --------------------
+  let socket = io("/");
+
+  let localPeer = new Peer(undefined, {
+    secure: true
+    host: 'localpeerjsserver.herokuapp.com',
+    port: 1801,
+
+  });
+
+  localPeer.on('open', (id)=> {
+    peerID = id;
+  });
+
+  let to_createRoomBtn = document.getElementById("to_createRoomBtn");
+  let to_joinRoomBtn = document.getElementById("to_joinRoomBtn");
+  let createRoomBtn = document.getElementById('createRoomBtn');
+  let joinRoomBtn = document.getElementById('joinRoomBtn');
+  let backBtn_crc = document.getElementById('cr_back_btn');
+  let backBtn_jrc = document.getElementById('jr_back_btn');
+  let message_input_element = document.getElementById("chat_input");
+  let sendBtn = document.getElementById("sendBtn");
+
+  let homePageContainer = document.getElementById("home_page_container");
+  let optionBtnsContainer = document.getElementById("to_optionBtn_container");
+  let createRoomContainer = document.getElementById("to_createRoom_container");
+  let joinRoomContainer = document.getElementById("to_joinRoom_container");
+  let exitBtn = document.getElementById("exit_btn");
+
+  let video_container = document.getElementById("video_page_container");
+  let video_element = document.createElement("video");
+  video_element.muted = true;
+  let vid_div = document.getElementById("first_user");
+  let vid_div2 = document.getElementById("second_user");
+
+  const addVideo = (video, stream) =>
+    {
+      video.srcObject = stream;
+      video.addEventListener('loadedmetadata', ()=> {
+        video.play();
+      });
+      $(vid_div).append($(video));
+  }
+
+  const addRemoteVideo = (video, stream) =>
+  {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', ()=> {
+      video.play();
+    });
+    $(vid_div2).append($(video));
+  }
+
+  let hasGetUserMedia = () =>
+  {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  }
+  //navigation -------------------
+  $(createRoomContainer).hide();
+  $(joinRoomContainer).hide();
+  $(video_container).hide();
+
+  $(to_createRoomBtn).on('click', ()=> {
+    $(optionBtnsContainer).hide();
+    $(createRoomContainer).fadeIn();
+  });
+
+  $(to_joinRoomBtn).on('click', ()=> {
+    $(optionBtnsContainer).hide();
+    $(joinRoomContainer).fadeIn();
+  });
+
+  $(backBtn_crc).on('click', ()=> {
+    $(createRoomContainer).hide();
+    $(optionBtnsContainer).fadeIn();
+  });
+
+  $(backBtn_jrc).on('click', ()=> {
+    $(joinRoomContainer).hide();
+    $(optionBtnsContainer).fadeIn();
+  });
+  /* -------------------------------------------- */
+  $(createRoomBtn).on('click', ()=> {
+    let roomName;
+    if($('#createRoom_roomname').val() != "")
+    {
+      roomName =  $('#createRoom_roomname').val();
+      if(peerID == ""){peerID=""}
+      socket.emit('createRoom',{roomName: roomName, userID: peerID});
+      $('#createRoom_roomname').val("");
+    }
+  });
+
+  $(joinRoomBtn).on('click', ()=> {
+    let roomName;
+    if($("#joinRoom_roomname").val() != "")
+    {
+      roomName = $("#joinRoom_roomname").val();
+      if(peerID == ""){peerID = ""}
+      socket.emit('enterRoom', {roomName: roomName, userID: peerID});
+      $("#joinRoom_roomname").val("");
+    }
+  });
+  /*------------------------------------------------- */
+  socket.on("userJoined", data => {
+    $(homePageContainer).hide();
+    $(video_container).fadeIn();
+    roomLog["name"] = data["name"];
+    roomLog["roomUserInfo"] = data["roomUserInfo"];
+    $("#room_info").text(`room: ${roomLog["name"]}`);
+    $("#chat_box").append(`<div><p>user joined room</p></div>`);
+
+    if(data["user"] == 1)
+    {
+      if(hasGetUserMedia())
+      {
+        navigator.mediaDevices.getUserMedia(constraints).then((stream)=>{
+          addVideo(video_element, stream);
+
+          localPeer.on('call', call => {
+            call.answer(stream);
+            let video = document.createElement("video");
+            call.on('stream', remoteStream => {
+              addRemoteVideo(video, remoteStream);
+            })
+          })
+        });
+      }
+    }
+
+  });
+
+  socket.on('userTwoConnected', (userID) => {
+    let tmp_id = userID;
+    console.log(tmp_id);
+    if(hasGetUserMedia())
+    {
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        addVideo(video_element, stream);
+        let video = document.createElement("video");
+        let call = localPeer.call(tmp_id, stream);
+        call.on('stream', remoteStream => {
+          addRemoteVideo(video, remoteStream);
+        })
+        call.on('close', () => {
+          video.remove();
+        });
+        peers[tmp_id] = call;
+      });
+    }
+  });
+
+  $(sendBtn).on('click', ()=> {
+    if($(message_input_element).val() != "")
+    {
+      let message = $(message_input_element).val();
+      socket.emit("textMessage", {roomName: roomLog["name"], message: message});
+      $(message_input_element).val("");
+    }
+  });
+
+  socket.on("message", data => {
+    $("#chat_box").append(`<div><p>${data}</p></div>`);
+  });
+
+  /* Errors when joining a room */
+  socket.on("room not found", ()=> {
+    alert("room not found");
+  });
+
+  socket.on("room empty", ()=> {
+    alert("room empty");
+  });
+
+  socket.on("room is full", ()=> {
+    alert("room full");
+  });
+  //---------------------------------------------------------
+  $(exitBtn).on('click', ()=> {
+    console.log("exit button clicked + " + roomLog["name"]);
+    socket.emit('leaving', roomLog["name"]);
+  });
+
+  socket.on("user_left", ()=> {
+    $("#chat_box").append(`<div><p>user left room</p></div>`);
+  });
+
+  socket.on('cleanUpPage', ()=> {
+    $("#chat_box").empty();
+    $(video_page_container).hide();
+    $(homePageContainer).fadeIn();
+  });
+
+}//---end
---
+++ server.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const http = require("http").createServer(app);
+const io = require('socket.io')(http);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.route('/').get((req, res)=> {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+let users = 0;
+let roomLog = {};
+
+
+io.on('connection', socket => {
+
+  socket.on('createRoom', (data) => {
+    let roomName = data["roomName"];
+    socket.rooms = roomName;
+    socket.join(roomName);
+    let socketUserID_ojb = {"user1" : socket.id};
+    roomLog[roomName] = {userCount: 1, userIDs: [data["userID"]], socketIDs: [socketUserID_ojb]};
+    io.in(roomName).emit("userJoined", {name: roomName, roomUserInfo: roomLog[roomName], user: 1});
+  });
+
+  socket.on('enterRoom', data => {
+    let roomName = data["roomName"];
+    let room = io.sockets.adapter.rooms.get(roomName);
+    let clients = 0;
+    if(room)
+    {
+      clients = io.sockets.adapter.rooms.get(roomName).size;
+      if(clients == 0)
+      {
+          socket.emit('room empty');
+          return;
+      }
+      else if(clients > 1)
+      {
+        socket.emit('room is full');
+        return;
+      }
+      else
+      {
+        socket.join(roomName);
+        //roomLog[roomName] = {userCount: 1, userIDs: [socket.id]};
+        roomLog[roomName]["userCount"] += 1;
+        roomLog[roomName]["socketIDs"][0]["user2"] = socket.id;
+        roomLog[roomName]["userIDs"].push(data["userID"]);
+        io.in(roomName).emit("userJoined",{name: roomName, roomUserInfo: roomLog[roomName]});
+        io.in(roomName).emit("userTwoConnected", roomLog[roomName]["userIDs"][0]);
+      }
+    }
+    else
+    {
+      socket.emit('room not found');
+      return;
+    }
+  });
+
+  socket.on("textMessage", data => {
+    let roomName = data["roomName"];
+    let message = data["message"];
+    io.in(roomName).emit("message", message);
+  });
+
+  socket.on("leaving", (data)=> {
+    console.log(roomLog);
+    let roomName = data;
+    io.in(roomName).emit("user_left");
+    roomLog[roomName]["userCount"] -= 1;
+    socket.emit('cleanUpPage');
+    socket.leave(roomName);
+  });
+});//end of connection
+
+http.listen(3000, ()=> {
+  console.log('Listening on port: ' + 3000);
+});
